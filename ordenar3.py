348a81c14f84daf9299af2dc14b6e5364c8a85c1
angelicalbarradiazz-stack <angelicalbarradiazz@gmail.com>
2025-09-19 04:11:43 -0600

    www

--- ordenar3.py
+++ ordenar3.py
@@ -1,15 +1,44 @@
 """
 Inserta el encabezado aquí y escribe tu código abajo
+
+Dados tres números, ordenarlos de mayor a menor.
+    
+    Args:
+        a (float): Primer número real.
+        b (Float): Segundo número real.
+        c (float): Tercer número real.
+
+
+    Returns:
+        Tuple: lista de números ordenada del mayor al menor
+
+
+
+
 """
 
 # Declaraciones
 CONSTANTE = valor
+primernumero=input("Primer número ")
+segundonumero=input("Primer número ")
+tercernumero=input("Primer número ")
+
 
 # Entradas
 entrada = input()
 
+a=float(primernumero)
+b=float(segundonumero)
+c=float(tercernumero)
+
 # Proceso
+
+if a=b and b=c
+    print(("Primer número: ")(a)+("Segundo número ")+(b)+(c))
+
+
+
 
 
 # Salidas
-print(salida)
+#print(salida)
